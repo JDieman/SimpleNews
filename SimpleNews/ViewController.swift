c5c6fc2617dd94ca2195751e2d171e8f63eed093
Dmitry <jdieman@mail.ru>
2019-02-14 17:57:14 +0300

    Create XCode project

---
+++ SimpleNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleNews
+//
+//  Created by Dmitry Polyakov on 14/02/2019.
+//  Copyright © 2019 Dmitry Polyakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
